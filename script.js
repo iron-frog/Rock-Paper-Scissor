33f5ec8c15f1d8a20ae834c95c298d9e7d728010
iron-frog <124222884+iron-frog@users.noreply.github.com>
2023-02-23 23:28:35 +0000

    add a play function
    Changed the getComputerChoice() to output a string instead of
    numbers and added a play function for only player choice of rock

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
-function getComputerChoice(){
-    let choice = Math.floor(Math.random()*3);
-    return choice;
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() * 3);
+    if (choice == 0) {
+        return "rock";
+    }
+    if (choice == 1) {
+        return "paper";
+    }
+    if (choice == 2) {
+        return "scissor";
+    }
+
 }
 
-console.log(getComputerChoice());
+function play(playSelection, computerSelection) {
+    let player1 = playSelection.toUpperCase();
+    let player2 = computerSelection.toUpperCase();
+    let result = (player1 == "ROCK" && player2 == "PAPER") 
+    ?    "You lose! Paper beats Rock"
+    :   (player1 == "ROCK" && player2 == "SCISSOR")
+    ?   "You win! Rock beats Scissor"
+    :   "You draw!"
+
+    return result;
+
+}
+
+const computerSelection = getComputerChoice();
+const computerSelection1 = getComputerChoice();
+const computerSelection2 = getComputerChoice();
+
+console.log(play("rock", computerSelection));
+console.log(play("rock", computerSelection1));
+console.log(play("rock", computerSelection2));
